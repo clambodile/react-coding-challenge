3095c27e2d3fa46145fb975836214bd3c2bf2611
John Jacob <jacobjohnrobert@gmail.com>
2020-03-11 17:17:20 -0500

    Use SnackbarContent for notifications.

--- src/components/message-column.js
+++ src/components/message-column.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 
 function MessageColumn ({messages, label, priority, clearMessage}) {
         return (
-                <div>
+                <>
                 <h3>{label} Type {priority}</h3>
                     <p>Count: {messages.length}</p>
                 {messages.map(({message: content, id}, i) => (
                         <Grid key={i} xs={10} item>
-                        <Paper  >
-                        {content}
-                        <a onClick={e => {console.log("hi"); clearMessage(id); e.preventDefault()}}>
-                        Clear
-                    </a>
-                        </ Paper>
+                        <SnackbarContent message={content} action={(
+                                <Button size="small" onClick={e => {clearMessage(id); e.preventDefault()}}>
+                            Clear
+                                                                   </Button>)
+                                                                  } />
                         </Grid>
                 ))}
             
-                </div>
+                </>
         )
 }
 
