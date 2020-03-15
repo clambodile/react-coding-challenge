f5c347ae2e45746d611fcdee1e8108e077f45c5b
John Jacob <jacobjohnrobert@gmail.com>
2020-03-15 16:44:19 -0400

    give notifications their colors

--- src/components/message-column.js
+++ src/components/message-column.js
@@ -5,25 +5,42 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import SnackbarContent from '@material-ui/core/SnackbarContent';
 
+const useStyles = makeStyles({
+    message: {
+        marginBottom: "5px",
+        color: "black"
+    },
+    error: {
+        backgroundColor: "#F56236"
+    },
+    warning: {
+        backgroundColor: "#FCE788"
+    },
+    info: {
+        backgroundColor: "#88FCA3"
+    }
+})
+
 function MessageColumn ({messages, label, priority, clearMessage}) {
-        return (
+    const classes = useStyles()
+    return (
             <>
-                <h3>{label} Type {priority}</h3>
-                    <p>Count: {messages.length}</p>
-                <Grid container spacing={1} height="100%">
-                {messages.map(({message: content, id}, i) => (
-                        <Grid key={i} xs={10} item>
-                        <SnackbarContent message={content} action={(
-                                <Button size="small" onClick={e => {clearMessage(id); e.preventDefault()}}>
+            <h3>{label} Type {priority}</h3>
+            <p>Count: {messages.length}</p>
+            <Grid  container height="100%">
+            {messages.map(({message: content, id}, i) => (
+                    <Grid key={i} xs={10} item>
+                    <SnackbarContent className={`${classes.message} ${classes[label.toLowerCase()]}`} message={content} action={(
+                            <Button size="small" onClick={e => {clearMessage(id); e.preventDefault()}}>
                             Clear
-                                                                   </Button>)
-                                                                  } />
-                        </Grid>
-                ))}
-            
-                </ Grid>
-                </>
-        )
+                        </Button>)
+                                                                                          } />
+                    </Grid>
+            ))}
+
+        </ Grid>
+            </>
+    )
 }
 
 export default MessageColumn
