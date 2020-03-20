15796435ef28f735cf4340d10a401146beba2908
John Jacob <jacobjohnrobert@gmail.com>
2020-03-20 11:01:03 -0400

    Color and position error snackbar.

--- src/components/error-snackbar.js
+++ src/components/error-snackbar.js
@@ -1,30 +1,48 @@
-import React from 'react'
-import Button from '@material-ui/core/Button'
-import Snackbar from '@material-ui/core/Snackbar'
-import IconButton from '@material-ui/core/IconButton'
-import CloseIcon from '@material-ui/icons/Close'
+import React from "react"
+import Snackbar from "@material-ui/core/Snackbar"
+import IconButton from "@material-ui/core/IconButton"
+import CloseIcon from "@material-ui/icons/Close"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+  snackBar: {
+    marginTop: "-20px",
+    backgroundColor: "#F56236",
+    color: "black"
+  }
+})
 
 export default function ErrorSnackbar(props) {
-  const [open, setOpen] = React.useState(false)
+  const classes = useStyles()
 
   return (
     <div>
       <Snackbar
         anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
+          vertical: "top",
+          horizontal: "center"
+        }}
+        ContentProps={{
+          classes: {
+            root: classes.snackBar
+          }
         }}
         open={props.open}
         onClose={props.handleClose}
         message="Error"
         action={
           <>
-              <IconButton size="small" aria-label="close" color="inherit" onClick={props.handleClose}>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={props.handleClose}
+            >
               <CloseIcon fontSize="small" />
             </IconButton>
           </>
         }
       />
     </div>
-  );
+  )
 }
